8be10229e735a02d95517d4637a78040d9a43855
ss1119 <y.sasaki111919@gmail.com>
2023-04-19 20:30:06 +0900

    害虫説明画面の追加

--- src/components/views/Home.tsx
+++ src/components/views/Home.tsx
@@ -1,11 +1,13 @@
 import { Header } from "../parts/Header";
 import { TopPageContents } from "../parts/TopPageContents";
+import { Descriptions } from "../templates/Descriptions";
 
 export const Home = () => {
   return (
     <div>
       <Header />
       <TopPageContents />
+      <Descriptions />
     </div>
   );
 };
