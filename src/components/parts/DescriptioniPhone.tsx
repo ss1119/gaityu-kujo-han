097c3d8cdcdab55084e162c32805479ad56db2e2
ss1119 <y.sasaki111919@gmail.com>
2023-04-24 18:46:01 +0900

    iPhone用の説明コンポーネント作成

---
+++ src/components/parts/DescriptioniPhone.tsx
@@ -0,0 +1,60 @@
+import { DescriptionEntity } from "../../types";
+import { RedStar } from "./RedStar";
+import { WhiteStar } from "./WhiteStart";
+
+export const DescriptioniPhone = (props: DescriptionEntity) => {
+  const images = props.damageImages.map((image, index) => {
+    return (
+      <div key={index} className="mx-1">
+        <img src={image} className="border border-black" />
+      </div>
+    );
+  });
+
+  const risk = [];
+  for (let i = 1; i <= 5; i++) {
+    if (i <= props.risk) {
+      risk.push(<RedStar key={i} />);
+    } else {
+      risk.push(<WhiteStar key={i} />);
+    }
+  }
+  return (
+    <div className="visible rounded-lg bg-white w-auto p-4 h-auto mx-2 mb-5 md:hidden">
+      <div className="flex flex-col items-center">
+        <div className="flex items-center">
+          <div className="border border-black bg-white h-42 w-44 mr-4">
+            <p className="border-b border-black text-center font-bold">
+              {props.name}
+            </p>
+            <img src={props.animalImage} />
+            <p className="flex border-t border-black text-center justify-center items-center text-sm">
+              危険度：
+              {risk}
+            </p>
+          </div>
+          <div className="pt-1 w-32">
+            <p className="inline-block border-double font-bold text-sm">
+              駆除料金
+            </p>
+            <p className="inline-block border-double font-bold text-sm">
+              {props.lowestPrice}円〜{props.highestPrice}円
+            </p>
+          </div>
+        </div>
+        <div className="border border-black bg-white text-sm mt-3 mx-3">
+          <p className="border-b border-black text-center">
+            {props.name}の特徴
+          </p>
+          {props.discription}
+        </div>
+        <div className="pt-2">
+          <p className="inline-block border-double font-bold">
+            {props.name}による被害例
+          </p>
+        </div>
+        <div className="flex justify-between mt-3">{images}</div>
+      </div>
+    </div>
+  );
+};
--- src/components/templates/Descriptions.tsx
+++ src/components/templates/Descriptions.tsx
@@ -1,29 +1,38 @@
-import { Description } from "../parts/Description";
+import { DescriptionPC } from "../parts/DescriptionPC";
 import { descriptionContents } from "../../utils/descriptions";
+import { DescriptioniPhone } from "../parts/DescriptioniPhone";
 
 export const Descriptions = () => {
   const descriptions = descriptionContents.map((value, index) => {
     return (
-      <Description
-        key={index}
-        name={value.name}
-        lowestPrice={value.lowestPrice}
-        highestPrice={value.highestPrice}
-        risk={value.risk}
-        discription={value.discription}
-        animalImage={value.animalImage}
-        damageImages={value.damageImages}
-      />
+      <div key={index}>
+        <DescriptionPC
+          name={value.name}
+          lowestPrice={value.lowestPrice}
+          highestPrice={value.highestPrice}
+          risk={value.risk}
+          discription={value.discription}
+          animalImage={value.animalImage}
+          damageImages={value.damageImages}
+        />
+        <DescriptioniPhone
+          name={value.name}
+          lowestPrice={value.lowestPrice}
+          highestPrice={value.highestPrice}
+          risk={value.risk}
+          discription={value.discription}
+          animalImage={value.animalImage}
+          damageImages={value.damageImages}
+        />
+      </div>
     );
   });
   return (
     <div className="flex flex-col items-center bg-gray-200 md:mt-24 md:pt-24">
-      <div className="text-center pt-3 font-bold tex-red-600 bg-yellow-300 text-2xl md:w-56 md:h-16 md:mt-20 md:text-4xl">
+      <div className="flex justify-center items-center p-2 mt-5 font-bold tex-red-600 bg-yellow-300 text-2xl md:w-56 md:h-16 md:mt-20 md:text-4xl">
         対象の害虫
       </div>
-      <div className="md:grid md:grid-cols-2 md:pt-8 md:px-">
-        {descriptions}
-      </div>
+      <div className="pt-2 md:grid md:grid-cols-2 md:pt-8">{descriptions}</div>
     </div>
   );
 };
