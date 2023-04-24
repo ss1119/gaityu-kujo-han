1c70d469471a1dc40236fe77c5571948c81815fd
ss1119 <y.sasaki111919@gmail.com>
2023-04-24 18:19:09 +0900

    fix layout

--- src/utils/descriptions.tsx
+++ src/utils/descriptions.tsx
@@ -7,7 +7,7 @@ export const descriptionContents: DescriptionEntity[] = [
     highestPrice: 15000,
     risk: 4,
     discription: (
-      <p className="text-sm text-left md:p-2">
+      <p className="text-sm text-left p-2">
         アシナガバチは人間が先に攻撃しない限り、基本的に攻撃性は低いとされていますが、危険な事に変わりはありません。
         <br />
         しかし、スズメバチやオオスズメバチは人間が近づくだけでも驚異的な攻撃性を見せ、刺されてしまうとアナフィラキシーショックを引き起こし、最悪の場合死に至ってしまう、非常に危険な害虫です。
@@ -28,7 +28,7 @@ export const descriptionContents: DescriptionEntity[] = [
     highestPrice: 20000,
     risk: 3,
     discription: (
-      <p className="text-sm text-left md:p-2">
+      <p className="text-sm text-left p-2">
         アシナガバチは人間が先に攻撃しない限り、基本的に攻撃性は低いとされていますが、危険な事に変わりはありません。
         <br />
         しかし、スズメバチやオオスズメバチは人間が近づくだけでも驚異的な攻撃性を見せ、刺されてしまうとアナフィラキシーショックを引き起こし、最悪の場合死に至ってしまう、非常に危険な害虫です。
@@ -49,7 +49,7 @@ export const descriptionContents: DescriptionEntity[] = [
     highestPrice: 10000,
     risk: 5,
     discription: (
-      <p className="text-sm text-left md:p-2">
+      <p className="text-sm text-left p-2">
         アシナガバチは人間が先に攻撃しない限り、基本的に攻撃性は低いとされていますが、危険な事に変わりはありません。
         <br />
         しかし、スズメバチやオオスズメバチは人間が近づくだけでも驚異的な攻撃性を見せ、刺されてしまうとアナフィラキシーショックを引き起こし、最悪の場合死に至ってしまう、非常に危険な害虫です。
@@ -70,7 +70,7 @@ export const descriptionContents: DescriptionEntity[] = [
     highestPrice: 25000,
     risk: 3,
     discription: (
-      <p className="text-sm text-left md:p-2">
+      <p className="text-sm text-left p-2">
         アシナガバチは人間が先に攻撃しない限り、基本的に攻撃性は低いとされていますが、危険な事に変わりはありません。
         <br />
         しかし、スズメバチやオオスズメバチは人間が近づくだけでも驚異的な攻撃性を見せ、刺されてしまうとアナフィラキシーショックを引き起こし、最悪の場合死に至ってしまう、非常に危険な害虫です。
