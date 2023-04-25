5c5b7627bc43b1652d769dd07e483cf60b76eb7f
ss1119 <y.sasaki111919@gmail.com>
2023-04-25 21:07:46 +0900

    PC用画面作成

--- src/components/views/Home.tsx
+++ src/components/views/Home.tsx
@@ -1,6 +1,7 @@
 import { Header } from "../parts/Header";
 import { TopPageContents } from "../parts/TopPageContents";
 import { Descriptions } from "../templates/Descriptions";
+import { Reason } from "../templates/Reason";
 
 export const Home = () => {
   return (
@@ -8,6 +9,7 @@ export const Home = () => {
       <Header />
       <TopPageContents />
       <Descriptions />
+      <Reason />
     </div>
   );
 };
