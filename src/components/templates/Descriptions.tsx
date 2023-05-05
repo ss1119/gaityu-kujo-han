8dd1df4d09eefce91dec7c8bea831586dd29ad1d
ss1119 <y.sasaki111919@gmail.com>
2023-05-05 17:44:29 +0900

    fix

--- src/components/templates/Descriptions.tsx
+++ src/components/templates/Descriptions.tsx
@@ -28,9 +28,9 @@ export const Descriptions = () => {
     );
   });
   return (
-    <div className="flex flex-col items-center bg-gray-200 pb-5 md:mt-24 md:pt-24 md:pb-10">
-      <div className="flex justify-center items-center p-2 mt-5 font-bold bg-yellow-300 text-2xl md:px-5 md:mt-20 md:text-5xl">
-        対象の害虫
+    <div className="flex flex-col items-center bg-gray-200 pb-5 md:pt-14 md:pb-10">
+      <div className="flex justify-center items-center p-2 mt-5 font-bold bg-yellow-300 text-xl md:px-5 md:mt-20 md:text-5xl">
+        対象の害虫・害獣
       </div>
       <div className="pt-2 md:grid md:grid-cols-2 md:pt-5">{descriptions}</div>
     </div>
