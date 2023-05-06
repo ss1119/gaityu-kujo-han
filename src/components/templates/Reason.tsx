2a9aac3d47bde8321b5be17416de0eed61ef49b7
ss1119 <y.sasaki111919@gmail.com>
2023-05-06 19:31:56 +0900

    選ばれる理由画面iPad対応

--- src/components/templates/Reason.tsx
+++ src/components/templates/Reason.tsx
@@ -4,7 +4,7 @@ export const Reason = () => {
   return (
     <div className="flex flex-col items-center reason-bg">
       <div className="flex justify-center items-center p-2 mt-2 font-bold bg-yellow-300 h-10 text-2xl lg:px-5 lg:h-16 lg:mt-14 lg:text-5xl">
-        害虫駆除班が選ばれる理由
+        ホームプロテクターが選ばれる理由
       </div>
       <div className="flex justify-center items-center font-bold bg-black text-white px-2 mt-1 lg:px-5 lg:mt-2 lg:h-12 lg:text-3xl">
         お客様の快適な生活を守ります
