041991e0c0b98eac7b5affa4dec2b1b2b20a6e3a
ss1119 <y.sasaki111919@gmail.com>
2023-05-18 13:25:23 +0900

    fix layout

--- src/components/parts/CheckBox.tsx
+++ src/components/parts/CheckBox.tsx
@@ -4,14 +4,9 @@ type Props = {
 
 export const CheckBox = (props: Props) => {
   return (
-    <div className="flex items-center">
-      <img
-        src="assets/checkbox.png"
-        className="w-12 xs:w-8 md:w-16 md:ml-10 xl:w-24 xl:ml-24"
-      />
-      <p className="font-bold xs:text-xs md:text-xl lg:text-2xl xl:text-3xl">
-        {props.text}
-      </p>
+    <div className="flex items-center justify-center">
+      <img src="assets/checkbox.png" className="w-12 xs:w-8 md:w-16 xl:w-24" />
+      <p className="font-bold xs:text-xs md:text-3xl">{props.text}</p>
     </div>
   );
 };
