9623fa2310ca82f56705d917b358d7fca6434959
ss1119 <y.sasaki111919@gmail.com>
2023-05-18 13:25:36 +0900

    fix neglect page

--- src/components/templates/Neglect.tsx
+++ src/components/templates/Neglect.tsx
@@ -1,4 +1,4 @@
-import { CheckBox } from "../parts/CheckBox";
+import { NeglectItem } from "./NeglectItem";
 
 export const Neglect = () => {
   return (
@@ -6,17 +6,49 @@ export const Neglect = () => {
       <div className="flex justify-center items-center p-2 font-bold bg-blue-300 text-lg xs:text-sm xs:py-1 md:text-4xl md:px-4 lg:px-5 lg:text-5xl">
         害虫・害獣被害を放置するとどうなる？
       </div>
-      <div className="flex">
-        <div className="flex flex-col pt-2 md:pt-8 lg:pt-16 lg:mr-10">
-          <CheckBox text="糞尿により天井や壁から悪臭がする" />
-          <CheckBox text="動物が出す騒音により眠れない" />
-          <CheckBox text="ダニや寄生虫が大量発生する" />
-          <CheckBox text="お子様やペットに危害が及ぶ可能性がある" />
-          <CheckBox text="アレルギーや感染症になるリスクがある" />
-          <CheckBox text="動物が天井で亡くなり腐敗する" />
-          <CheckBox text="動物が子供を産んで住み着いてしまう" />
-          <CheckBox text="最悪の場合、不動産価値を落としてしまう" />
-          <div className="flex justify-center md:hidden">
+      <div className="w-screen">
+        <div className="flex flex-col pt-2 md:pt-8 lg:pt-16 xl:px-10">
+          <div className="xl:flex xl:pb-14">
+            <NeglectItem
+              text="糞尿により天井や壁から悪臭がする"
+              fileName="neglect1.png"
+            />
+            <NeglectItem
+              text="動物が出す騒音により眠れない"
+              fileName="neglect2.png"
+            />
+          </div>
+          <div className="xl:flex xl:pb-14">
+            <NeglectItem
+              text="ダニや寄生虫が大量発生する"
+              fileName="neglect3.png"
+            />
+            <NeglectItem
+              text="お子様やペットに危害が及ぶ可能性がある"
+              fileName="neglect4.png"
+            />
+          </div>
+          <div className="xl:flex xl:pb-14">
+            <NeglectItem
+              text="アレルギーや感染症になるリスクがある"
+              fileName="neglect5.png"
+            />
+            <NeglectItem
+              text="動物が天井で亡くなり腐敗する"
+              fileName="neglect6.png"
+            />
+          </div>
+          <div className="xl:flex xl:pb-14">
+            <NeglectItem
+              text="動物が子供を産んで住み着いてしまう"
+              fileName="neglect7.png"
+            />
+            <NeglectItem
+              text="最悪の場合、不動産価値を落としてしまう"
+              fileName="neglect8.png"
+            />
+          </div>
+          {/* <div className="flex justify-center md:hidden">
             <div className="w-40 mt-5 mr-5 xs:w-28">
               <img
                 src="assets/neglects/neglect1.png"
@@ -43,7 +75,7 @@ export const Neglect = () => {
               src="assets/neglects/neglect2.png"
               className="border-4 border-white rounded-md"
             />
-          </div>
+          </div>*/}
         </div>
       </div>
     </div>
---
+++ src/components/templates/NeglectItem.tsx
@@ -0,0 +1,20 @@
+import { CheckBox } from "../parts/CheckBox";
+
+type Props = {
+  text: string;
+  fileName: string;
+};
+
+export const NeglectItem = (props: Props) => {
+  return (
+    <div className="flex-col pb-8 md:pb-12 xl:pb-0 xl:w-1/2">
+      <CheckBox text={props.text} />
+      <div className="flex justify-center pt-3">
+        <img
+          src={"assets/neglects/" + props.fileName}
+          className="w-52 border-4 border-white rounded-md md:w-96"
+        />
+      </div>
+    </div>
+  );
+};
