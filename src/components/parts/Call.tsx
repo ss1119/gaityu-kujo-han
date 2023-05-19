f1e5130761e895cc5f5fd633399d8ce4a0c235b4
ss1119 <y.sasaki111919@gmail.com>
2023-05-19 22:55:05 +0900

    add new component

---
+++ src/components/parts/Call.tsx
@@ -0,0 +1,10 @@
+export const Call = () => {
+  return (
+    <a href="tel:0668857656">
+      <img
+        src="assets/call.png"
+        className="border-2 border-black rounded-md w-64 xs:border xs:w-18 md:w-80 lg:w-80 xl:w-64"
+      />
+    </a>
+  );
+};
