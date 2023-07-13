78c728cf0228fffeb10fdbd0c66f9e6a593246fa
ss1119 <y.sasaki111919@gmail.com>
2023-07-13 16:22:08 +0900

    fix images

--- src/components/parts/Call.tsx
+++ src/components/parts/Call.tsx
@@ -2,7 +2,7 @@ export const Call = () => {
   return (
     <a href="tel:0668857656">
       <img
-        src="assets/call.png"
+        src="assets/call.jpg"
         className="shadow-md border-2 border-black rounded-md w-64 xs:border xs:w-18 md:w-80 lg:w-80 xl:w-64"
       />
     </a>
