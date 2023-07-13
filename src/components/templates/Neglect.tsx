a36b9a2270778b950e3e106e815eb1829296dc3e
ss1119 <y.sasaki111919@gmail.com>
2023-07-13 16:14:18 +0900

    fix images

--- src/components/templates/Neglect.tsx
+++ src/components/templates/Neglect.tsx
@@ -11,48 +11,48 @@ export const Neglect = () => {
           <div className="xl:flex xl:pb-14">
             <NeglectItem
               text="糞尿により天井や壁から悪臭がする"
-              neglectFileName="neglect1.png"
+              neglectFileName="neglect1.jpg"
               commentFileName="comment1.png"
             />
             <NeglectItem
               text="動物が出す騒音により眠れない"
-              neglectFileName="neglect2.png"
+              neglectFileName="neglect2.jpg"
               commentFileName="comment2.png"
             />
           </div>
           <div className="xl:flex xl:pb-14">
             <NeglectItem
               text="ダニや寄生虫が大量発生する"
-              neglectFileName="neglect3.png"
+              neglectFileName="neglect3.jpg"
               commentFileName="comment3.png"
             />
             <NeglectItem
               text="お子様やペットに危害が及ぶ可能性がある"
-              neglectFileName="neglect4.png"
+              neglectFileName="neglect4.jpg"
               commentFileName="comment4.png"
             />
           </div>
           <div className="xl:flex xl:pb-14">
             <NeglectItem
               text="アレルギーや感染症になるリスクがある"
-              neglectFileName="neglect5.png"
+              neglectFileName="neglect5.jpg"
               commentFileName="comment5.png"
             />
             <NeglectItem
               text="動物が天井で亡くなり腐敗する"
-              neglectFileName="neglect6.png"
+              neglectFileName="neglect6.jpg"
               commentFileName="comment6.png"
             />
           </div>
           <div className="xl:flex xl:pb-14">
             <NeglectItem
               text="動物が子供を産んで住み着いてしまう"
-              neglectFileName="neglect7.png"
+              neglectFileName="neglect7.jpg"
               commentFileName="comment7.png"
             />
             <NeglectItem
               text="最悪の場合、不動産価値を落としてしまう"
-              neglectFileName="neglect8.png"
+              neglectFileName="neglect8.jpg"
               commentFileName="comment8.png"
             />
           </div>
