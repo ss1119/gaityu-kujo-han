85112f1187b0484fb8473787abc9805ab6d39bd7
ss1119 <y.sasaki111919@gmail.com>
2023-07-15 14:36:40 +0900

    DOMサイズを小さくする

--- src/components/parts/RedStar.tsx
+++
@@ -1,9 +0,0 @@
-export const RedStar = () => {
-  return (
-    <img
-      src="/assets/star-red.webp"
-      alt="star-red"
-      className="w-auto h-3 md:h-5 lg:h-4"
-    />
-  );
-};
--- src/components/parts/WhiteStart.tsx
+++
@@ -1,9 +0,0 @@
-export const WhiteStar = () => {
-  return (
-    <img
-      src="/assets/star-white.webp"
-      alt="star-white"
-      className="w-auto h-3 md:h-5 lg:h-4"
-    />
-  );
-};
