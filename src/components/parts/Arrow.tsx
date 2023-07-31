b9f8fecc6d41775620976955bee368601e35888d
ss1119 <y.sasaki111919@gmail.com>
2023-07-31 15:58:54 +0900

    下向き矢印のコンポーネント追加

---
+++ src/components/parts/Arrow.tsx
@@ -0,0 +1,13 @@
+export const Arrow = () => {
+  return (
+    <div className="flex justify-center">
+      <img
+        src="assets/arrow-down.webp"
+        alt="矢印"
+        className="w-12 py-0.5 xs:w-7 md:w-20 md:py-1"
+        width={50}
+        height={70}
+      />
+    </div>
+  );
+};
