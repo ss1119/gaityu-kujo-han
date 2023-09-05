28d51417b83949f5c08b4254fb065c2e21046f33
ss1119 <y.sasaki111919@gmail.com>
2023-09-05 18:05:14 +0900

    fix

--- src/components/templates/Pages.tsx
+++ src/components/templates/Pages.tsx
@@ -3,27 +3,35 @@ import { Call } from "../parts/Call";
 export const Pages = () => {
   return (
     <div>
-      <img
-        src="assets/pages/page1.webp"
-        className="w-full"
-        alt="害虫の駆除は専門家にお任せください。アシナガバチ、オオスズメバチ、ゴキブリ、トコジラミなどの害虫に対応しております。当日契約で駆除料金50%OFF"
-      />
+      <div className="flex justify-center">
+        <img
+          src="assets/pages/page1.webp"
+          className="w-full lg:w-1/2"
+          alt="害虫の駆除は専門家にお任せください。アシナガバチ、オオスズメバチ、ゴキブリ、トコジラミなどの害虫に対応しております。当日契約で駆除料金50%OFF"
+        />
+      </div>
       <Call />
-      <img
-        src="assets/pages/page2.webp"
-        className="w-full"
-        alt="他社との大きな違いはビジネスモデルです。仲介業者を挟まないため仲介手数料が発生せず、より安い料金で駆除を承ることができます。"
-      />
-      <img
-        src="assets/pages/page3.webp"
-        className="w-full"
-        alt="悪徳駆除業社にご注意ください。"
-      />
-      <img
-        src="assets/pages/page4.webp"
-        className="w-full"
-        alt="ホームプロテクターなら、お見積り料金にご満足いただいた後に作業を開始させていただきます。また、再発駆除のご連絡にも迅速に対応させていただきます。"
-      />
+      <div className="flex justify-center">
+        <img
+          src="assets/pages/page2.webp"
+          className="w-full lg:w-1/2"
+          alt="他社との大きな違いはビジネスモデルです。仲介業者を挟まないため仲介手数料が発生せず、より安い料金で駆除を承ることができます。"
+        />
+      </div>
+      <div className="flex justify-center">
+        <img
+          src="assets/pages/page3.webp"
+          className="w-full lg:w-1/2"
+          alt="悪徳駆除業社にご注意ください。"
+        />
+      </div>
+      <div className="flex justify-center">
+        <img
+          src="assets/pages/page4.webp"
+          className="w-full lg:w-1/2"
+          alt="ホームプロテクターなら、お見積り料金にご満足いただいた後に作業を開始させていただきます。また、再発駆除のご連絡にも迅速に対応させていただきます。"
+        />
+      </div>
     </div>
   );
 };
