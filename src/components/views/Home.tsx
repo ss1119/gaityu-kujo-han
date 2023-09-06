bb9409d1ac0a232a8ad9edb92b81064079ba79a8
ss1119 <y.sasaki111919@gmail.com>
2023-09-06 14:54:53 +0900

    delete components

--- src/components/views/Home.tsx
+++ src/components/views/Home.tsx
@@ -1,14 +1,8 @@
 import { Company } from "../parts/Company";
-import { Estimate } from "../parts/Estimate";
 import { Footer } from "../parts/Footer";
 import { Header } from "../parts/Header";
-import { Damage } from "../templates/Damage";
 import { Descriptions } from "../templates/Descriptions";
 import { Explain } from "../parts/Explain";
-import { Neglect } from "../templates/Neglect";
-import { Reason } from "../templates/Reason";
-import { RequestFlow } from "../templates/RequestFlow";
-import { Review } from "../templates/Review";
 import { Pages } from "../templates/Pages";
 import { Examples } from "../parts/Examples";
 
@@ -20,12 +14,6 @@ export const Home = () => {
       <Explain />
       <Descriptions />
       <Examples />
-      <Estimate />
-      <RequestFlow />
-      <Reason />
-      <Damage />
-      <Neglect />
-      <Review />
       <Company />
       <Footer />
     </>
